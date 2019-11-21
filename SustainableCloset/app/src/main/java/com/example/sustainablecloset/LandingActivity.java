dc9545ef292bb8e68dd916e0e49e6ede3977d2ba
Jorelle <jorellekebeto@gmail.com>
2019-11-21 12:04:43 -0500

    Link login and landing page

--- SustainableCloset/app/src/main/java/com/example/sustainablecloset/LandingActivity.java
+++ SustainableCloset/app/src/main/java/com/example/sustainablecloset/LandingActivity.java
@@ -1,7 +1,14 @@
 package com.example.sustainablecloset;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class LandingActivity {
+public class LandingActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.landing_page);
+    }
 }
