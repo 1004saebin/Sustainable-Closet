e10bd01675a1bd92d861987143f1abe1493747a8
Jorelle <jorellekebeto@gmail.com>
2019-12-02 23:16:58 -0500

    Camera access

--- SustainableCloset/app/src/main/java/com/example/sustainablecloset/AddImagesActivity.java
+++ SustainableCloset/app/src/main/java/com/example/sustainablecloset/AddImagesActivity.java
@@ -1,20 +1,25 @@
 package com.example.sustainablecloset;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -45,30 +50,54 @@ public class AddImagesActivity extends AppCompatActivity {
     }
 
     public void openOption(View view){
-        showPictureDialog();
+        showPictureDialog(AddImagesActivity.this);
     }
 
-    private void showPictureDialog(){
-        AlertDialog.Builder pictureDialog = new AlertDialog.Builder(this);
-        pictureDialog.setTitle("Select Action");
-        String[] pictureDialogItems = {
-                "Select photo from gallery",
-                "Capture photo from camera" };
-        pictureDialog.setItems(pictureDialogItems,
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        switch (which) {
-                            case 0:
-                                choosePhotoFromGallery();
-                                break;
-                            case 1:
-                                takePhotoFromCamera();
-                                break;
-                        }
-                    }
-                });
-        pictureDialog.show();
+    private void showPictureDialog(Context context){
+//        AlertDialog.Builder pictureDialog = new AlertDialog.Builder(this);
+//        pictureDialog.setTitle("Select Action");
+//        String[] pictureDialogItems = {
+//                "Select photo from gallery",
+//                "Capture photo from camera" };
+//        pictureDialog.setItems(pictureDialogItems,
+//                new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        switch (which) {
+//                            case 0:
+//                                choosePhotoFromGallery();
+//                                break;
+//                            case 1:
+//                                takePhotoFromCamera();
+//                                break;
+//                        }
+//                    }
+//                });
+//        pictureDialog.show();
+        final CharSequence[] options = {"Take Photo", "Choose from Gallery", "Cancel"};
+
+        android.app.AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Choose your profile picture");
+
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+
+                if (options[item].equals("Take Photo")) {
+                    Intent takePicture = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(takePicture, 0);
+
+                } else if (options[item].equals("Choose from Gallery")) {
+                    Intent pickPhoto = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(pickPhoto, 1);//one can be replaced with any action code
+
+                } else if (options[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
     }
 
     public void choosePhotoFromGallery() {
@@ -137,45 +166,100 @@ public class AddImagesActivity extends AppCompatActivity {
         return imageFile;
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == this.RESULT_CANCELED) {
-            return;
-        }
-        if (requestCode == GALLERY) {
-            if (data != null) {
-
-                //address of the image
-                Uri contentURI = data.getData();
-
-
-                try {
-                    //get the bitmap
-                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), contentURI);
-
-                    String path = saveImage(bitmap);
-                    Toast.makeText(AddImagesActivity.this, "Image Saved!", Toast.LENGTH_SHORT).show();
-
-                    //show the image
-                    imageview.setImageBitmap(bitmap);
-
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    Toast.makeText(AddImagesActivity.this, "Failed!", Toast.LENGTH_SHORT).show();
-                }
-            }
-
-        } else if (requestCode == CAMERA) {
-////            Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+//    @Override
+//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
 //
-//            //show the image
-//            imageview.setImageBitmap(thumbnail);
-//            saveImage(thumbnail);
-            Toast.makeText(AddImagesActivity.this, "Image Saved!", Toast.LENGTH_SHORT).show();
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if (resultCode == this.RESULT_CANCELED) {
+//            return;
+//        }
+//        if (requestCode == GALLERY) {
+//            if (data != null) {
+//
+//                //address of the image
+//                Uri contentURI = data.getData();
+//
+//
+//                try {
+//                    //get the bitmap
+//                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), contentURI);
+//
+//                    String path = saveImage(bitmap);
+//                    Toast.makeText(AddImagesActivity.this, "Image Saved!", Toast.LENGTH_SHORT).show();
+//
+//                    //show the image
+//                    imageview.setImageBitmap(bitmap);
+//
+//
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                    Toast.makeText(AddImagesActivity.this, "Failed!", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//
+//        } else if (requestCode == CAMERA) {
+//////            Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+////
+////            //show the image
+////            imageview.setImageBitmap(thumbnail);
+////            saveImage(thumbnail);
+//            Toast.makeText(AddImagesActivity.this, "Image Saved!", Toast.LENGTH_SHORT).show();
+//        }
+//    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != RESULT_CANCELED) {
+            switch (requestCode) {
+                case 0:
+                    if (resultCode == RESULT_OK && data != null) {
+                        Bitmap selectedImage = (Bitmap) data.getExtras().get("data");
+                        imageview.setImageBitmap(selectedImage);
+                    }
+
+                    break;
+                case 1:
+                    if (resultCode == RESULT_OK && data != null) {
+                        Uri selectedImage = data.getData();
+                        String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                        if (selectedImage != null) {
+                            Cursor cursor = getContentResolver().query(selectedImage,
+                                    filePathColumn, null, null, null);
+                            if (cursor != null) {
+                                cursor.moveToFirst();
+
+                                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                                String picturePath = cursor.getString(columnIndex);
+                                imageview.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+                                cursor.close();
+                            }
+                        }
+
+                    }
+                    break;
+            }
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_image_capture, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public String saveImage(Bitmap myBitmap) {
