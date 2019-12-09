9d8c81305f235e270caec5df2e0a2fd45020182e
chidiu98 <34973870+koji98@users.noreply.github.com>
2019-12-09 16:31:40 -0500

    Updated outfit recommender activity

--- SustainableCloset/app/src/main/java/com/example/sustainablecloset/OutfitRecommenderActivity.java
+++ SustainableCloset/app/src/main/java/com/example/sustainablecloset/OutfitRecommenderActivity.java
@@ -7,12 +7,23 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class OutfitRecommenderActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.outfit_recommender);
+    }
+
+    public ArrayList<String> getOutfits(){
+        ArrayList<String> ImgArr = new ArrayList<>();
+        ImgArr.add("./outfit_recommend_1.jpeg");
+        ImgArr.add("./outfit_recommend_2.jpeg");
+        ImgArr.add("./outfit_recommend_3.jpeg");
+        ImgArr.add("./outfit_recommend_4.jpeg");
+        return ImgArr;
     }
 
 
--- SustainableCloset/app/src/main/java/com/example/sustainablecloset/StackView.java
+++ SustainableCloset/app/src/main/java/com/example/sustainablecloset/StackView.java
@@ -1,4 +1,31 @@
 package com.example.sustainablecloset;
 
-public class StackView {
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.Nullable;
+
+public class StackView extends View {
+
+    private ArrayList<String> ImgArr = new ArrayList<>();
+
+    public StackView(Context context) {
+        super(context);
+        ImgArr.add("./outfit_recommend_1.jpeg");
+    }
+
+    public StackView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public StackView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+
 }
